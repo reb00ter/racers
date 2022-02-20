b080998e715a2b3e42edbf7ad572e9c233c727fd
Maxim Barabanov <maxim.barabanov@gmail.com>
2022-02-20 23:46:16 +0300

    racers photos

--- internal/models/racer.go
+++ internal/models/racer.go
@@ -5,7 +5,7 @@ import "time"
 type Racer struct {
 	ID        string `gorm:"type:uuid;primary_key;default:gen_random_uuid()"`
 	Name      string `sql:"type:varchar(100)"`
-	Image     string `sql:"type:varchar(100)"`
+	Image     string `sql:"type:varchar(500)"`
 	Rating    int64  `sql:"type:bigint"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
